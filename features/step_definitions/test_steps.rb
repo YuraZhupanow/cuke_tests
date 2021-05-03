bccdf423db5e67bb61bcb6273c7d2e48a0f50f91
YurriZ <yura.zhupanow@gmail.com>
2021-05-03 13:39:22 +0300

    added empty step definitions

---
+++ features/step_definitions/test_steps.rb
@@ -0,0 +1,75 @@
+Given('I am not logged in visitor') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I register {string} user via Redmine {string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see the {string} user is registered') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I become logged in as {string} user') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I create a project') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see that project is created on {string} level') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I add {string} user as a member of the project') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I can can see {string} user in the project member list') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I create an issue and assign {string} user to created issue') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see the issue is created') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see {string} user is assigned to the issue') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I logout') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I login as {string} user') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I track time for the assigned issue') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see the time is tracked properly') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I close the issue') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see the issue was closed') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I close the project') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see it was successfully closed') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
